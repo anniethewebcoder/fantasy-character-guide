d46b9d807ae367ae11b70c6fd4ce81c0e636e85f
anniethewebcoder <anniethewebcoder@gmail.com>
2025-02-13 11:24:52 -0500

    set up register

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,9 @@
 const User = require("./../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnuthenticatedError } = require("./../errors");
+
+const register = async (req, res) => {};
+
+const login = async (req, res) => {};
+
+module.exports = { register, login };
---
+++ public/register.js
@@ -0,0 +1,3 @@
+export const handleRegister = () => {};
+
+export const showRegister = () => {};
