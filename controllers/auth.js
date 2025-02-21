e0ebef26e3a72c568b38ac41c2fc05a88735f907
Annie <annie.the.web.coder@gmail.com>
2025-02-21 16:15:44 -0500

    user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
-const User = require("./../models/User");
+const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, UnuthenticatedError } = require("./../errors");
+const { BadRequestError, UnuthenticatedError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
   const user = await User.create({
@@ -20,6 +20,25 @@ const login = async (req, res) => {
   if (!email || !password) {
     throw new BadRequestError("Please provide email and password");
   }
+
+  const user = await User.findOne({ email })
+
+  if(!user) {
+    throw new UnauthenticatedError("Invalid Credentials")
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+
+  if(!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials")
+  }
+
+  const token = user.createJWT()
+
+  res.status(StatusCodes.OK).json({
+    user: { name: user.name },
+    token
+  })
 };
 
 module.exports = { register, login };
