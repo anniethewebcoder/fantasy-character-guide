cc9eb4a5ee9b4e77ea3d6707ffabe02de872a571
anniethewebcoder <anniethewebcoder@gmail.com>
2025-02-25 13:32:55 -0500

    register button on about

--- public/index.js
+++ public/index.js
@@ -1,49 +1,8 @@
-let activeDiv = null;
+let token = null;
 
-export const setDiv = (newDiv) => {
-  if (newDiv != activeDiv) {
-    if (activeDiv) {
-      activeDiv.style.display = "none";
-    }
-
-    newDiv.style.display = "block";
-    activeDiv = newDiv;
-  }
-};
-
-export let inputEnabled = true;
-
-export const enableInput = (state) => {
-  inputEnabled = state;
-};
-
-export let token = null;
-
-export const setToken = (value) => {
-  token = value;
-
-  if (value) {
-    localStorage.setItem("token", value);
-  } else {
-    localStorage.removeItem("token");
-  }
-};
-
-import { showUser } from "./user";
-import { showCharacter } from "./character";
-import { handleRegister } from "./register";
-
-document.addEventListener("DOMContentLoaded", () => {
+if (localStorage.getItem("token")) {
   token = localStorage.getItem("token");
-
-  handleLogin();
-  handleRegister();
-
-  if (token) {
-    showUser();
-    showCharacter();
-    showJournal();
-  } else {
-    showLoginRegister();
-  }
-});
+  setTimeout(function () {
+    window.location.href = "character.html";
+  }, 2000);
+}
