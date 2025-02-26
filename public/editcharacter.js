f3cc19c9845dfb8821ef388b7361eb1afc20e4f0
Annie <annie.the.web.coder@gmail.com>
2025-02-26 14:45:17 -0500

    edit value

--- public/editcharacter.js
+++ public/editcharacter.js
@@ -5,6 +5,34 @@ const charclass = document.getElementById("charclass");
 const charbackground = document.getElementById("charbackground");
 const charButton = document.getElementById("create-button");
 
+const params = window.location.search
+const id = new URLSearchParams(params).get('id')
+
 document.addEventListener("DOMContentLoaded", async (e) => {
-    token = localStorage.getItem("token")
+    const token = localStorage.getItem("token")
+
+    e.preventDefault()
+
+    try {
+        const response = await fetch(`/api/v1/character/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        const data = await response.json()
+
+        const { name, age, species, classes, backgrounds} = data.character
+
+        charname.value = name;
+        charage.value = age;
+        charclass.value = classes
+        charbackground.value = backgrounds
+        charspecies.value = species
+    } catch (error) {
+        console.error(error)
+    }
+
 })
