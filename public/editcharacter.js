b0efa6a9691daff5f96e4ba6326a24bcb44f761c
Annie <annie.the.web.coder@gmail.com>
2025-02-26 22:35:53 -0500

    patch

--- public/editcharacter.js
+++ public/editcharacter.js
@@ -3,7 +3,7 @@ const charage = document.getElementById("charage");
 const charspecies = document.getElementById("charspecies");
 const charclass = document.getElementById("charclass");
 const charbackground = document.getElementById("charbackground");
-const charButton = document.getElementById("create-button");
+const editButton = document.getElementById("edit-button");
 
 const params = window.location.search
 const id = new URLSearchParams(params).get('id')
@@ -35,4 +35,41 @@ document.addEventListener("DOMContentLoaded", async (e) => {
         console.error(error)
     }
 
+})
+
+editButton.addEventListener("click", async (e) => {
+    e.preventDefault()
+
+    try {
+        const response = await fetch(`/api/v1/character/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+              body: JSON.stringify({
+                name: charname.value
+                  .split(" ")
+                  .map((w) => w.charAt(0).toUpperCase() + w.slice(1))
+                  .join(" "),
+                age: charage.value,
+                classes: charclass.value
+                  .split(" ")
+                  .map((w) => w.charAt(0).toUpperCase() + w.slice(1))
+                  .join(" "),
+                backgrounds: charbackground.value
+                  .split(" ")
+                  .map((w) => w.charAt(0).toUpperCase() + w.slice(1))
+                  .join(" "),
+                species: charspecies.value
+                  .split(" ")
+                  .map((w) => w.charAt(0).toUpperCase() + w.slice(1))
+                  .join(" "),
+              })
+        })
+
+        const data = await response.json()
+    } catch (error) {
+        console.error(error)
+    }
 })
